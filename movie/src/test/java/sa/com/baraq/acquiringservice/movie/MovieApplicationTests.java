a6b4a0c8579daf1283859b892ea2db117d1750cc
saraalhosin <SKAlhosin@baraq.com>
2026-05-05 16:50:53 +0300

    Initial commit

---
+++ movie/src/test/java/sa/com/baraq/acquiringservice/movie/MovieApplicationTests.java
@@ -0,0 +1,13 @@
+package sa.com.baraq.acquiringservice.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
